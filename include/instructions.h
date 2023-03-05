d464c5ce97936a2e458ad6f57be2eb9128ff64c0
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-03-05 08:11:49 +0000

    Update instructions.h

--- include/instructions.h
+++ include/instructions.h
@@ -6,8 +6,8 @@
 /* CREATING TYPE DEFINED METHODS TO INSTANTIATE RAW POINTERS FOR MY */
 /* METHODS, FUNCTIONS, ETC */
 
-#ifndef COMMON
-#define COMMON
+#ifndef INSTRUCTIONS
+#define INSTRUCTIONS
 
 #include "common.h"
 #include <stdint.h>
@@ -17,13 +17,30 @@
 /* THESE ARE STAITCALLY DECLARED OBJECTS */
 /* THAT DETERMINE THE INDEXING MODES OF THE INSTRUCTION */
 
-#ifndef CPU_TYPES
-#define CPU_TYPES
+#ifndef INSTRUCTION_TYPES
+#define INSTRUCTION_TYPES
 
-#define INSTRUCTION_ALIGN_UP(POINT_A, POINT_B)((UNK_*(POINT_A + UNK_*(POINT_B))
+#define INSTRUCTION_ALIGN_UP(POINT_A, POINT_B)((UNK_(POINT_A + UNK_*(POINT_B))
 #define INSTRUCTION_ALIGN_DOWN(POINT_A, POINT_B)((UNK_*(POINT_A + UNK_*(POINT_B) -1))
 #define INSTRUCTION_ALIGNED(POINT_A) __declspec(aligned(POINT_A))
 #define BIT_SIZE(X)(sizeof(X))
+
+#endif
+
+#ifndef SUBROUTINES
+#define SUBROUTINES
+
+#define ARGS(...)(__VA_ARGS__)
+#define NUM_ARGS(N)(UNK_*(&N))
+
+#endif
+
+#ifndef REGISTER_FUNCTIONS
+#define REGISTER_FUNCTIONS
+
+typedef void(*UPDATE_CR0(void));
+typedef void(*SET_SEGMENT_REGISTER(void));
+typedef void(*SET_CARRY_FLAG(void));
 
 #endif
 #endif
