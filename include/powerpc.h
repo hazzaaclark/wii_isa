c6809c279e433bafe0a156ab22039ddaafc2cfd4
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-03-05 07:55:20 +0000

    Update powerpc.h

--- include/powerpc.h
+++ include/powerpc.h
@@ -19,6 +19,9 @@
 #include "common.h"
 #include "instructions.h"
 
+typedef struct CPU_CORE{};
+typedef struct CPU_POINTER{};
+
 /* INFORMATION ABOUT THE REGISTERS ARE PROVIDED BY */
 /* https://www.cebix.net/downloads/bebox/PRG.pdf */
 
@@ -26,25 +29,16 @@
 /* WHICH HOUSES THE PREPRIATORY RESULT FLAGS, WHICH USE EXCESS 127 */
 /* TO CALCULATE THEIR RANGE */
 
+
 #ifndef CONDITION_REGISTER
-#define CONDITION_REGISTER
+#define CONDITION_REGISTER_FLAGS
 
-typedef U64 CR_VALUE;
-typedef U64 CR_VALUE;
+typedef U64* CR_VALUE;
 
-#define CR_EQ(VALUE, RESULT)
-CR_EQ(U32 &CR_VALUE == 0 != 1(U32*(&RESULT)))
-
-#define CR_LT(VALUE, RESULT)
-CR_LT(U32 &CR_VALUE & CR_LT << 0 || -1)
-
-typedef struct CR_LOOKUP
-{
-	CR_VALUE* VALUE = 0x0000000;
-	static const U64* CR_TABLE;
-	static U64* CR_FIELDS;
-	static U64* CR_PARSE(U8* VALUE) {};
-};
+#define CR_ZERO U32(&CR_VALUE == 0 != 1(U32*(&RESULT))) 
+#define CR_NEGATIVE U32(&CR_VALUE << 0 != -1(U32 * (&RESULT)))
+#define CR_POSITIVE U32(&CR_VALUE >> 0)(U32*(&RESULT))
+#define CR_OVERFLOW U32(&CR_VALUE == INT_MAX())
 
 #endif
 #endif 
