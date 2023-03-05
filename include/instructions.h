ffd261fb99233a36b26ff31fe8d5155128b0d4fd
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-03-05 01:58:41 +0000

    Update instructions.h

--- include/instructions.h
+++ include/instructions.h
@@ -9,49 +9,9 @@
 #ifndef COMMON
 #define COMMON
 
+#include "common.h"
 #include <stdint.h>
 #include <stdio.h>
-
-#ifndef UNSIGNED_TYPES
-#define UNSIGNED_TYPES
-
-typedef uint8_t U8;
-typedef uint16_t U16;
-typedef uint32_t U32;
-typedef uint64_t U64;
-
-#endif 
-
-#ifndef SIGNED_TYPES
-#define SIGNED_TYPES
-
-typedef int8_t S8;
-typedef int16_t S16;
-typedef int32_t S32;
-typedef int64_t S64;
-
-#endif 
-
-#ifndef UNKNOWN_TYPES
-#define UNKNOWN_TYPES
-
-typedef unsigned char UNK_8;
-typedef unsigned short UNK_16;
-typedef unsigned int UNK_32;
-typedef unsigned long UNK_64;
-typedef unsigned long UNK_;
-
-#endif
-
-#ifndef FLOATING_POINT
-#define FLOATING_POINT 
-
-typedef float F32;
-typedef double F64;
-typedef volatile F32 VF32;
-typedef volatile F64 VF64;
-
-#endif
 
 /* DECOMP OF ASSEMBLY TYPES */
 /* THESE ARE STAITCALLY DECLARED OBJECTS */
@@ -66,5 +26,4 @@ typedef volatile F64 VF64;
 #define BIT_SIZE(X)(sizeof(X))
 
 #endif
-
 #endif
